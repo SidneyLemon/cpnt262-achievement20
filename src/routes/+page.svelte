a8241e5061b3299fee16eea4708b4dcb84d85a45
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-22 14:51:19 -0600

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<main>
+  <h1>CPNT 262: Achievement 20</h1>
+  <ul>
+    <li>
+      <a href="/something-here/">something-here</a>
+    </li>
+  </ul>
+</main>
---
+++ src/routes/[param]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from "$app/stores";
+  console.log($page.param);
+</script>
+
+<main>
+  <h1>{$page.params.param}</h1>
+</main>
